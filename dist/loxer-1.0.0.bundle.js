08b9816f3fd0d52bc587be886bbfcfc6680e493c
lassev05 <vestergaardlasse2@gmail.com>
2022-08-25 21:37:30 +0200

    Fix build script

---
+++ dist/loxer-1.0.0.bundle.js
@@ -0,0 +1,6 @@
+/*
+    * v1.0.0
+    * Copyright (c) 2022 
+    * Licensed under the ISC license 
+*/
+"use strict";function lox(o){console.log(o)}
--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,25 @@
-var version = require("../package.json").version
-var user = require("../package.json").author
+var version = require("../package.json").version;
+var author = require("../package.json").author;
+var license = require("../package.json").license;
 
+const time = Date.now();
 
-const time = Date.now()
-
-
-require("esbuild").build({
+require("esbuild")
+  .build({
     entryPoints: ["./src/browser.js"],
     bundle: false,
+    banner: {
+      js: `/*
+    * v${version}
+    * Copyright (c) 2022 ${author}
+    * Licensed under the ${license} license 
+*/`,
+    },
+    minify: true,
     outfile: `./dist/loxer-${version}.bundle.js`,
-}).then(() => {
-    console.log(`Build completed in ${Date.now() - time}ms`)
-}).catch(() => process.exit(1))
+  })
+
+  .then(() => {
+    console.log(`Build completed in ${Date.now() - time}ms`);
+  })
+  .catch(() => process.exit(1));
